46bef5d1219a7470d26a95da0e68af1e0ab8882c
Skye Adamson <skye.b.adamson@protonmail.com>
2022-06-10 15:32:35 +0100

    Implements further challenge methods for starting challenges and getting active challenge

--- ChallengeManager.py
+++ ChallengeManager.py
@@ -21,6 +21,7 @@ class ChallengeManager:
     def __init__(self, game_instance):
         self.game_instance = game_instance
         self.completed_challenges = []
+        self.active_challenge = None
 
     def get_completed_challenges(self):
         if self.game_instance.BrowserManager.load_challenges():
@@ -32,3 +33,23 @@ class ChallengeManager:
             return completed_challenges
         else:
             return []
+
+    def get_active_challenge(self):
+        if self.game_instance.BrowserManager.load_challenges():
+            for challenge_id in self.challenge_conversion_table.keys():
+                challenge_button = self.game_instance.driver.find_element(By.ID, f"challenge{self.challenge_conversion_table[challenge_id]}")
+                if challenge_button.get_attribute("class") == "onchallengebtn":
+                    return challenge_id
+            return None
+        return None
+
+    def start_challenge(self, challenge_id):
+        if self.game_instance.BrowserManager.load_challenges():
+            challenge_button = self.game_instance.driver.find_element(By.ID, f"challenge{self.challenge_conversion_table[challenge_id]}")
+            if challenge_button.get_attribute("class") != "onchallengebtn":
+                challenge_button.click()
+                return True
+            else:
+                return True
+        else:
+            return False
--- Main.py
+++ Main.py
@@ -6,7 +6,8 @@ if game.BrowserManager.start_browser():
     game.disable_sacrifice_confirmation()
     game.InfinityManager.purchased_upgrades = game.InfinityManager.get_purchased_infinity_upgrades()
     game.ChallengeManager.completed_challenges = game.ChallengeManager.get_completed_challenges()
-    print(game.ChallengeManager.completed_challenges)
+    game.ChallengeManager.active_challenge = game.ChallengeManager.get_active_challenge()
+    print(game.ChallengeManager.active_challenge)
     game.BrowserManager.load_dimensions()
     while game.debug_mode == False:
         if game.InfinityManager.big_crunch():
